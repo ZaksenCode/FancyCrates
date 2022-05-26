448c3006b20b9c3d2ce4d45381a61e34508db35b
zaDR0tic <maksimsidorkin2006@mail.ru>
2022-05-26 16:00:00 +0300

    Crate

--- src/main/java/com/zaksen/fancydecorativeblocks/screen/FancyMenuTypes.java
+++ src/main/java/com/zaksen/fancydecorativeblocks/screen/FancyMenuTypes.java
@@ -3,15 +3,12 @@ package com.zaksen.fancydecorativeblocks.screen;
 import com.zaksen.fancydecorativeblocks.FancyDecorativeBlocks;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraftforge.common.extensions.IForgeBlockEntity;
 import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.IContainerFactory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
-import java.awt.*;
 
 public class FancyMenuTypes {
     public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.CONTAINERS, FancyDecorativeBlocks.MOD_ID);
